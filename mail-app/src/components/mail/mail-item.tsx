2eac494ff888434588de307f56a316ed6276c80a
caveman <anshumans09@hotmail.com>
2024-12-17 21:15:51 +0530

    added more buttons to mail-item component

--- mail-app/src/components/mail/mail-item.tsx
+++ mail-app/src/components/mail/mail-item.tsx
@@ -8,8 +8,12 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { Checkbox } from "../ui/checkbox";
+import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
 import { selectedEmailState } from "./atoms/mail-atoms";
 import { useSetRecoilState } from "recoil";
+import { Button } from "../ui/button";
+import { Flag, MailOpen, PinIcon, Trash2 } from "lucide-react";
 
 const EmailCard = (mail: Email) => {
     const setSelectedEmail = useSetRecoilState(selectedEmailState);
@@ -20,16 +24,23 @@ const EmailCard = (mail: Email) => {
 
     return (
         <Card className="mt-4 h-20 transition-transform duration-100 hover:scale-105 hover:bg-orange-50 rounded-sm" onClick={cardClicked}>
-            <CardHeader>
-                <CardTitle>{mail.sender}</CardTitle>
-                <CardDescription>{mail.subject}</CardDescription>
-            </CardHeader>
-            <CardContent>
-
-            </CardContent>
-            <CardFooter>
-
-            </CardFooter>
+            <div className="flex h-full pl-5 m-0">
+                <Checkbox className="my-auto" />
+                <div className="mt-3">
+                    <CardTitle className="text-start pl-5">{mail.sender}</CardTitle>
+                    <CardDescription className="text-start pl-5">{mail.subject}</CardDescription>
+                </div>
+                <div className="ml-auto mt-0 flex">
+                    <ToggleGroup type='multiple' className="items-start">
+                        <ToggleGroupItem value="mail-open"> <MailOpen /> </ToggleGroupItem>
+                        <ToggleGroupItem value="flag"> <Flag /> </ToggleGroupItem>
+                        <ToggleGroupItem value="pin"> <PinIcon /> </ToggleGroupItem>
+                    </ToggleGroup>
+                    <Button variant={'ghost'} className="py-[39px] px-3 hover:bg-red-400 hover:text-white rounded-none">
+                        <Trash2 className="p-0" />
+                    </Button>
+                </div>
+            </div>
         </Card>
     )
 }
